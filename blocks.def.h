1399778fa972860232148871b44b4c3dd892ae99
Dusan Ilic <idtronicsrs@gmail.com>
2021-04-16 17:24:23 +0200

    Initial commit

---
+++ blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+
+	{"  ", "~/software/dwmblocks/scripts/memory",	        6,		            1},
+
+	{"  ", "~/software/dwmblocks/scripts/volume",			2,		            10},
+
+	{"  ", "~/software/dwmblocks/scripts/clock",			5,		            0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
