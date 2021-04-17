30939c47b37292d5c68003ab308b367a6b8d9f83
Dusan Ilic <idtronicsrs@gmail.com>
2021-04-17 22:07:15 +0200

    Changed icons and scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+	{"",     "~/software/dwmblocks/scripts/memory",    5,    1},
 
-	{"  ", "~/software/dwmblocks/scripts/memory",	        6,		            1},
+	{"",     "~/software/dwmblocks/scripts/volume",    0,    10},
 
-	{"  ", "~/software/dwmblocks/scripts/volume",			2,		            10},
+	{"",     "~/software/dwmblocks/scripts/battery",   60,   11},
 
-	{"", "~/software/dwmblocks/scripts/battery",			5,		            10},
+	{"",     "~/software/dwmblocks/scripts/date",      3600, 12},
 
-	{"  ", "~/software/dwmblocks/scripts/date",			5,		            10},
-
-	{"  ", "~/software/dwmblocks/scripts/clock",			5,		            0},
+	{"",     "~/software/dwmblocks/scripts/clock",     60,   0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
